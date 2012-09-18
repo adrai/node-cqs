aa1195a86e50644963253944d87ec9183c8e528b
Adriano Raiano <araiano@ksw.kaba.com>
2012-09-18 13:30:19 +0200

    fix

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ index.aggregateBase = require('./lib/domain/bases/aggregateBase');
 index.commandHandlerBase = require('./lib/domain/bases/commandHandlerBase');
 index.sagaBase = require('./lib/domain/bases/sagaBase');
 index.sagaHandlerBase = require('./lib/domain/bases/sagaHandlerBase');
-index.eventDenormalizerBase = require('node-cqrs-eventdenormalizer').eventDenormalizerBase;
-index.eventExtenderBase = require('node-cqrs-eventdenormalizer').eventExtenderBase;
+index.eventDenormalizerBase = require('cqrs-eventdenormalizer').eventDenormalizerBase;
+index.eventExtenderBase = require('cqrs-eventdenormalizer').eventExtenderBase;
